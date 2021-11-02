641d854d7966dcc375a05ed0756061a0241bad22
JR-1991 <30547301+JR-1991@users.noreply.github.com>
2021-11-02 15:31:04 +0100

    Added toDict method and parser

--- pyaniml/utility/utils.py
+++ pyaniml/utility/utils.py
@@ -7,6 +7,8 @@ from pydantic.json import pydantic_encoder
 
 from xsdata.formats.dataclass.serializers import XmlSerializer
 from xsdata.formats.dataclass.serializers.config import SerializerConfig
+from xsdata.formats.dataclass.context import XmlContext
+from xsdata.formats.dataclass.parsers import XmlParser
 
 
 class SchemaBase:
@@ -36,20 +38,56 @@ class SchemaBase:
             default=pydantic_encoder
         )
 
+    def toDict(self) -> str:
+        """Serializes an abstract data model to a dictionary.
+
+        Returns:
+            Dict[Str, Any]: JSON data model as string.
+        """
+        return pydantic_encoder(self)
+
     def JSONSchema(self) -> Dict[str, Any]:
         return self.__pydantic_model__.schema()
+
+    @classmethod
+    def fromXMLString(cls, xml_string: str):
+        """Initializes an object model from an XML string.
+
+        Args:
+            xml_string (str): XML data model as string.
+
+        Returns:
+            Any: Object model that has been parsed from the XML string.
+        """
+        parser = XmlParser(context=XmlContext())
+        return parser.from_string(xml_string, cls)
+
+    @classmethod
+    def fromJSONString(cls, json_string: str):
+        """Initializes an object model from a JSON string.
+
+        Args:
+            json_string (str): JSON data model as string.
+
+        Returns:
+            Any: Object model that has been parsed from the JSON string.
+        """
+        return cls.__pydantic_model__.parse_raw(json_string)
 
 
 def _generateField(
     name: str,
     type: str,
     default: Any = None,
-    choices: Dict[str, Any] = None
+    choices: Dict[str, Any] = None,
+    namespace: str = ""
 ):
     """Metafunction to generate a valid field for later XML serialization."""
 
     # Declare dict type
-    metadata_dict: Dict[str, Any] = dict(name=name, type=type)
+    metadata_dict: Dict[str, Any] = dict(
+        name=name, type=type, namespace=namespace
+    )
 
     if choices:
         metadata_dict['choices'] = choices
