478f425fdbd13470aa422c8f0f16ffd65ccd7551
Jan Range <30547301+JR-1991@users.noreply.github.com>
2022-12-30 14:19:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pyAnIML",
-    version="0.0.2",
+    version="1.0.1",
     description="Handling of AnIML files",
     url="https://github.com/FAIRChemistry/pyAnIML",
     author="Range, Jan",
